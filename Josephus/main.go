2640ecf9c82294f758ad38cb66b77649b73a0be5
謝佳忻 <r9857723@gmail.com>
2021-11-25 23:09:20 +0800

    Josephus add + show

--- Josephus/main.go
+++ Josephus/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import "fmt"
 
+type Boy struct {
+	no int
+	next *Boy
+}
+
+func AddBoy(num int) *Boy {
+	first := &Boy{}
+	curBoy := &Boy{}
+	if num < 1 {
+		return first
+	}
+
+	for i := 1; i <= num; i++ {
+		boy := &Boy{
+			no: i,
+		}
+		if i == 1 {
+			first = boy
+		} else {
+			curBoy.next = boy
+		}
+		curBoy = boy
+		curBoy.next = first
+	}
+	return first
+}
+
+func ShowBoy(first *Boy) {
+	if first.next == nil {
+		fmt.Println("first is null")
+		return
+	}
+	tempBoy := first
+	for {
+		fmt.Println(tempBoy)
+		if tempBoy.next == first {
+			break
+		}
+		tempBoy = tempBoy.next
+	}
+}
+
+func main() {
+	link := AddBoy(5)
+
+	ShowBoy(link)
 }
