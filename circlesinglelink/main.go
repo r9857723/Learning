48b89048aa1541c1f6ee7cff156faff6bfac46bc
謝佳忻 <r9857723@gmail.com>
2021-11-25 22:28:13 +0800

    circlesinglelink delete

--- circlesinglelink/main.go
+++ circlesinglelink/main.go
@@ -9,7 +9,7 @@ type HeroNode struct {
 }
 
 // InsertHeroNode insert heroNode to link
-func (h *HeroNode) InsertHeroNode(newHeroNode *HeroNode) {
+func InsertHeroNode(h *HeroNode, newHeroNode *HeroNode) {
 	if h.next == nil {
 		h.no = newHeroNode.no
 		h.name = newHeroNode.name
@@ -19,7 +19,7 @@ func (h *HeroNode) InsertHeroNode(newHeroNode *HeroNode) {
 	}
 	tempNode := h
 	for {
-		if tempNode.next == tempNode {
+		if tempNode.next == h {
 			break
 		}
 		tempNode = tempNode.next
@@ -28,8 +28,61 @@ func (h *HeroNode) InsertHeroNode(newHeroNode *HeroNode) {
 	newHeroNode.next = h
 }
 
+func DeleteHeroNode(h *HeroNode, no int) *HeroNode {
+
+	tempNode := h
+	helperNode := h
+	//tempNode := h
+
+	// 空鏈表
+	if tempNode.next == nil {
+		fmt.Println("link is null")
+		return h
+	}
+	// 只有頭節點
+	if tempNode.next == h {
+		if tempNode.no == no {
+			tempNode.next = nil
+		}
+		return h
+	}
+
+	for {
+		if helperNode.next == h {
+			break
+		}
+		helperNode = helperNode.next
+	}
+
+	flag := true
+	for {
+		if tempNode.next == h {
+			break
+		}
+		if tempNode.no == no {
+			if tempNode == h {
+				h = h.next
+			}
+			helperNode.next = tempNode.next
+			flag = false
+			break
+		}
+		tempNode = tempNode.next
+		helperNode = helperNode.next
+	}
+	if flag {
+		if tempNode.no == no {
+			helperNode.next = tempNode.next
+		} else {
+			fmt.Println("no not exist")
+		}
+	}
+	return h
+}
+
 // ListHeroNode show all heroNode data
-func (h *HeroNode) ListHeroNode() {
+func ListHeroNode(h *HeroNode) {
+
 	tempNode := h
 	if tempNode.next == nil {
 		fmt.Println("link is null")
@@ -53,11 +106,21 @@ func main() {
 		name: "Tom",
 		next: nil,
 	}
-	head.InsertHeroNode(hero1)
+	InsertHeroNode(head, hero1)
 	hero2 := &HeroNode{
 		no: 2,
 		name: "Roman",
 	}
-	head.InsertHeroNode(hero2)
-	head.ListHeroNode()
+	InsertHeroNode(head, hero2)
+	hero3 := &HeroNode{
+		no: 3,
+		name: "Yuki",
+	}
+	InsertHeroNode(head, hero3)
+	ListHeroNode(head)
+
+	head = DeleteHeroNode(head, 1)
+	//head.DeleteHeroNode(2)
+	//head.DeleteHeroNode(3)
+	ListHeroNode(head)
 }
