b43f392949d6b82758bba1ee19436f8427c00718
謝佳忻 <r9857723@gmail.com>
2021-11-26 11:31:09 +0800

    josephus ok

--- Josephus/main.go
+++ Josephus/main.go
@@ -2,50 +2,117 @@ package main
 
 import "fmt"
 
-type Boy struct {
+type node struct {
 	no int
-	next *Boy
+	next *node
 }
 
-func AddBoy(num int) *Boy {
-	first := &Boy{}
-	curBoy := &Boy{}
+
+
+func CreateNodes(num int) *node {
+	firstNode := &node{}
+	tempNode := &node{}
 	if num < 1 {
-		return first
+		return firstNode
 	}
 
 	for i := 1; i <= num; i++ {
-		boy := &Boy{
+		boy := &node{
 			no: i,
 		}
 		if i == 1 {
-			first = boy
+			firstNode = boy
 		} else {
-			curBoy.next = boy
+			tempNode.next = boy
 		}
-		curBoy = boy
-		curBoy.next = first
+		tempNode = boy
+		tempNode.next = firstNode
 	}
-	return first
+	return firstNode
 }
 
-func ShowBoy(first *Boy) {
+func ShowNode(first *node) {
 	if first.next == nil {
 		fmt.Println("first is null")
 		return
 	}
-	tempBoy := first
+	tempNode := first
 	for {
-		fmt.Println(tempBoy)
-		if tempBoy.next == first {
+		fmt.Println(tempNode)
+		if tempNode.next == first {
 			break
 		}
-		tempBoy = tempBoy.next
+		tempNode = tempNode.next
 	}
 }
 
-func main() {
-	link := AddBoy(5)
+func Count(first *node) int {
+	if first.next == nil {
+		return 0
+	}
+	tempNode := first
+	var cnt int
+	for {
+		cnt++
+		if tempNode.next == first {
+			break
+		}
+		tempNode = tempNode.next
+	}
+	return cnt
+}
 
-	ShowBoy(link)
+func DoJosephus(h *node, startNum int, countNum int) {
+	if h.next == nil {
+		fmt.Println("link is not exist")
+		return
+	}
+
+	// check limit
+	if Count(h) < startNum {
+		fmt.Println("out of index")
+		return
+	}
+
+	//get last
+	tailNode := h
+	for {
+		if tailNode.next == h {
+			break 
+		}
+		tailNode = tailNode.next
+	}
+
+	// move to startNum	
+	for i := 0; i < startNum -1; i++ {
+		h = h.next
+		tailNode = tailNode.next
+	}
+	
+	for {
+		for i := 1; i <= countNum -1; i++ {
+			h = h.next
+			tailNode = tailNode.next
+		}
+
+
+		fmt.Print(h)
+		fmt.Println("Out")
+		h = h.next
+		tailNode.next = h
+
+		if h == tailNode {
+			break
+		}
+	}
+	fmt.Print(h)
+	fmt.Println("Out")
+
+}
+
+func main() {
+	nodes := CreateNodes(5)
+	ShowNode(nodes)
+	fmt.Println()
+	DoJosephus(nodes, 2, 3)
 }
